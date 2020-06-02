7c2b8c8f18af5bfb3457c49434345178c1ee9b9b
abuarquemf <abuarquemf@gmail.com>
2020-06-02 15:14:35 -0300

    added new fields

--- candidatura.go
+++ candidatura.go
@@ -2,6 +2,7 @@ package descritor
 
 import (
 	"encoding/json"
+	"time"
 
 	"github.com/gocarina/gocsv"
 )
@@ -17,9 +18,36 @@ const (
 
 // Candidatura representa uma
 type Candidatura struct {
-	Legislatura int    `json:"leg" csv:"leg"`     // Ano eleitoral em que a candidatura foi homologada.
-	Cargo       Cargo  `json:"cargo" csv:"cargo"` // Cargo sendo pleiteado pela candidatura.
-	UF          string `json:"uf" csv:"uf"`       // Identificador (2 caracteres) de unidade federativa.
+	Legislatura       int       `json:"leg" csv:"leg"`                               // Ano eleitoral em que a candidatura foi homologada.
+	Cargo             Cargo     `json:"cargo" csv:"cargo"`                           // Cargo sendo pleiteado pela candidatura.
+	UF                string    `json:"uf" csv:"uf"`                                 // Identificador (2 caracteres) de unidade federativa onde ocorreu a candidatura.
+	CPF               string    `json:"cpf" csv:"cpf"`                               // CPF do candidato.
+	Turno             int       `json:"turno" csv:"turno"`                           // Turno do pleito (1 ou 2).
+	Descricacao       string    `json:"descricao" csv:"desricao"`                    // Descrição da candidatura, informando o tipo e ano.
+	Data              time.Time `json:"data" csv:"data"`                             // Data que a eleição ocorreu.
+	Municipio         string    `json:"municipio" csv:"municipio"`                   // Município que ocorreu a eleição.
+	NumeroUrna        int       `json:"numero_urna" csv:"numero_urna"`               // Número do candidato na urna.
+	Nome              string    `json:"nome" csv:"nome"`                             // Nome de pessoa física do candidato.
+	NomeUrna          string    `json:"nome_urna" csv:"nome_urna"`                   // Nome do candidato na urna.
+	Email             string    `json:"email" csv:"email"`                           // Email do candidato.
+	Situacao          string    `json:"situacao" csv:"situacao"`                     // Situação da candidatura (podendo ser APTO ou INAPTO).
+	SituacaoCandidato string    `json:"situacao_candidato" csv:"situacao_candidato"` // Situação do candidato (pondendo ser DEFERIDO ou INDEFERIDO).
+	TipoAgremiacao    string    `json:"tipo_agremiacao" csv:"tipo_agremiacao"`       // Indica o tipo de agremiação do candidato (podendo ser PARTIDO ISOLADO ou AGREMIAÇÃO).
+	NumeroPartido     int       `json:"numero_partio" csv:"numero_partido"`          // Número do partido do candidato.
+	LegendaPartido    string    `json:"legenda_partido" csv:"legenda_partido"`       // Legenda do partido do candidato.
+	NomePartido       string    `json:"nome_partido" csv:"nome_partido"`             // Nome do partido do candidato.
+	NomeColigacao     string    `json:"nome_coligacao" csv:"nome_coligacao"`         // Nome da coligação a qual o candidato pertence.
+	PartidosColigacao string    `json:"partidos_coligacao" csv:"partidos_coligacao"` // Partidos pertencentes à coligação do candidato.
+	UFOrigem          string    `json:"uf_origem" csv:"uf_origem"`                   // Identificador (2 caracteres) da unidade federatirva de nascimento do candidato.
+	MunicipioOrigem   string    `json:"municipio_origem" csv:"municipio_origem"`     // Município de nascimento do candidato.
+	Nascimento        time.Time `json:"nascimento" csv:"nascimento"`                 // Data de nascimento do candidato.
+	TituloEleitoral   string    `json:"titulo_eleitoral" csv:"titulo_eleitoral"`     // Titulo eleitoral do candidato.
+	Gênero            string    `json:"genero" csv:"genero"`                         // Gênero do candidato (MASCULINO ou FEMININO).
+	GrauInstrucao     string    `json:"grau_instrucao" csv:"grau_instrucao"`         // Grau de instrução do candidato.
+	EstadoCivil       string    `json:"estado_civil" csv:"estado_civil"`             // Estado civil do candidato.
+	Raca              string    `json:"raca" csv:"raca"`                             // Raca do candidato (como BRANCA ou PARDA).
+	OcupacaoCandidato string    `json:"ocupacao" csv:"ocupacao"`                     // Ocupação do candidato (como COMERCIANTE).
+	SituacaoFinal     string    `json:"situacao_final" csv:"situacao_final"`         // Informa a situação ao fim da eleição (como ELEITO, NÃO ELEITO e SUPLENTE).
 }
 
 // ToJSON converte a candidatura para JSON.
--- candidatura_test.go
+++ candidatura_test.go
@@ -2,23 +2,136 @@ package descritor
 
 import (
 	"testing"
+	"time"
 
 	"github.com/matryer/is"
 )
 
 func TestCandidatura(t *testing.T) {
 	is := is.New(t)
-	cand := Candidatura{Legislatura: 2016, Cargo: ExecMunicipal, UF: "AL"}
+	data, err := time.Parse("02-01-2006", "02-10-2016")
+	if err != nil {
+		t.Errorf("want error nil when creating a test date, got %q", err)
+	}
+	nascimento, err := time.Parse("02-01-2006", "29-11-1997")
+	if err != nil {
+		t.Errorf("want error nil when creating a test date for nascimento, got %q", err)
+	}
+	cand := Candidatura{
+		Legislatura:       2016,
+		Cargo:             ExecMunicipal,
+		UF:                "AL",
+		CPF:               "07496470430",
+		Turno:             1,
+		Descricacao:       "Eleições Municipais 2016",
+		Data:              data,
+		Municipio:         "Município",
+		NumeroUrna:        45555,
+		Nome:              "Aurélio Buarque de Miranda Filho",
+		NomeUrna:          "Lelinho do povo",
+		Email:             "abuarquemf@gmail.com",
+		Situacao:          "APTO",
+		SituacaoCandidato: "DEFERIDO",
+		TipoAgremiacao:    "PARTIDO ISOLADO",
+		NumeroPartido:     45,
+		LegendaPartido:    "PSDB",
+		NomePartido:       "Partido Socilismo",
+		NomeColigacao:     "Todos pela cidade",
+		PartidosColigacao: "PSDB/PT/PSC",
+		UFOrigem:          "UF",
+		MunicipioOrigem:   "Maceio",
+		Nascimento:        nascimento,
+		TituloEleitoral:   "000000000",
+		Gênero:            "MASCULINO",
+		GrauInstrucao:     "ENSINO MEDIO COMPLETO",
+		EstadoCivil:       "SOLTEIRO",
+		Raca:              "PARDA",
+		OcupacaoCandidato: "ENGENHEIRO",
+		SituacaoFinal:     "ELEITO",
+	}
 	j, err := cand.ToJSON()
 	is.NoErr(err)
-	is.Equal(`{"leg":2016,"cargo":"EM","uf":"AL"}`, string(j))
+	is.Equal(`{"leg":2016,"cargo":"EM","uf":"AL","cpf":"07496470430","turno":1,"descricao":"Eleições Municipais 2016","data":"2016-10-02T00:00:00Z","municipio":"Município","numero_urna":45555,"nome":"Aurélio Buarque de Miranda Filho","nome_urna":"Lelinho do povo","email":"abuarquemf@gmail.com","situacao":"APTO","situacao_candidato":"DEFERIDO","tipo_agremiacao":"PARTIDO ISOLADO","numero_partio":45,"legenda_partido":"PSDB","nome_partido":"Partido Socilismo","nome_coligacao":"Todos pela cidade","partidos_coligacao":"PSDB/PT/PSC","uf_origem":"UF","municipio_origem":"Maceio","nascimento":"1997-11-29T00:00:00Z","titulo_eleitoral":"000000000","genero":"MASCULINO","grau_instrucao":"ENSINO MEDIO COMPLETO","estado_civil":"SOLTEIRO","raca":"PARDA","ocupacao":"ENGENHEIRO","situacao_final":"ELEITO"}`, string(j))
 
 }
 
 func TestToCSV(t *testing.T) {
-	c := []Candidatura{{Legislatura: 2016, Cargo: ExecMunicipal, UF: "AL"}, {Legislatura: 2016, Cargo: LegMunicipal, UF: "PE"}}
-	b, err := ToCSV(c)
+	data, err := time.Parse("02-01-2006", "02-10-2016")
+	if err != nil {
+		t.Errorf("want error nil when creating a test date, got %q", err)
+	}
+	nascimento, err := time.Parse("02-01-2006", "29-11-1997")
+	if err != nil {
+		t.Errorf("want error nil when creating a test date for nascimento, got %q", err)
+	}
+	c := []Candidatura{
+		{
+			Legislatura:       2016,
+			Cargo:             ExecMunicipal,
+			UF:                "AL",
+			CPF:               "07496470430",
+			Turno:             1,
+			Descricacao:       "Eleições Municipais 2016",
+			Data:              data,
+			Municipio:         "Município",
+			NumeroUrna:        45555,
+			Nome:              "Aurélio Buarque de Miranda Filho",
+			NomeUrna:          "Lelinho do povo",
+			Email:             "abuarquemf@gmail.com",
+			Situacao:          "APTO",
+			SituacaoCandidato: "DEFERIDO",
+			TipoAgremiacao:    "PARTIDO ISOLADO",
+			NumeroPartido:     45,
+			LegendaPartido:    "PSDB",
+			NomePartido:       "Partido Socialismo",
+			NomeColigacao:     "Todos pela cidade",
+			PartidosColigacao: "PSDB/PT/PSC",
+			UFOrigem:          "UF",
+			MunicipioOrigem:   "Maceio",
+			Nascimento:        nascimento,
+			TituloEleitoral:   "000000000",
+			Gênero:            "MASCULINO",
+			GrauInstrucao:     "ENSINO MEDIO COMPLETO",
+			EstadoCivil:       "SOLTEIRO",
+			Raca:              "PARDA",
+			OcupacaoCandidato: "ENGENHEIRO",
+			SituacaoFinal:     "ELEITO",
+		},
+		{
+			Legislatura:       2016,
+			Cargo:             ExecMunicipal,
+			UF:                "AL",
+			CPF:               "00000000000",
+			Turno:             1,
+			Descricacao:       "Eleições Municipais 2016",
+			Data:              data,
+			Municipio:         "Município",
+			NumeroUrna:        45555,
+			Nome:              "Marcela Moreira",
+			NomeUrna:          "Marcela guerreira",
+			Email:             "mmoreira@gmail.com",
+			Situacao:          "APTO",
+			SituacaoCandidato: "INDEFERIDO",
+			TipoAgremiacao:    "PARTIDO ISOLADO",
+			NumeroPartido:     45,
+			LegendaPartido:    "PSDB",
+			NomePartido:       "Partido Socialismo",
+			NomeColigacao:     "Todos em nome da cidade",
+			PartidosColigacao: "PSTU/PSB/PSL",
+			UFOrigem:          "UF",
+			MunicipioOrigem:   "Maceio",
+			Nascimento:        nascimento,
+			TituloEleitoral:   "123456789",
+			Gênero:            "FEMININO",
+			GrauInstrucao:     "ENSINO MEDIO INCOMPLETO",
+			EstadoCivil:       "CASADO",
+			Raca:              "NEGRO",
+			OcupacaoCandidato: "ARTISTA",
+			SituacaoFinal:     "SUPLENTE",
+		},
+	}
+	b, _ := ToCSV(c)
 	is := is.New(t)
 	is.NoErr(err)
-	is.Equal("leg,cargo,uf\n2016,EM,AL\n2016,LM,PE\n", string(b))
+	is.Equal("leg,cargo,uf,cpf,turno,desricao,data,municipio,numero_urna,nome,nome_urna,email,situacao,situacao_candidato,tipo_agremiacao,numero_partido,legenda_partido,nome_partido,nome_coligacao,partidos_coligacao,uf_origem,municipio_origem,nascimento,titulo_eleitoral,genero,grau_instrucao,estado_civil,raca,ocupacao,situacao_final\n2016,EM,AL,07496470430,1,Eleições Municipais 2016,2016-10-02T00:00:00Z,Município,45555,Aurélio Buarque de Miranda Filho,Lelinho do povo,abuarquemf@gmail.com,APTO,DEFERIDO,PARTIDO ISOLADO,45,PSDB,Partido Socialismo,Todos pela cidade,PSDB/PT/PSC,UF,Maceio,1997-11-29T00:00:00Z,000000000,MASCULINO,ENSINO MEDIO COMPLETO,SOLTEIRO,PARDA,ENGENHEIRO,ELEITO\n2016,EM,AL,00000000000,1,Eleições Municipais 2016,2016-10-02T00:00:00Z,Município,45555,Marcela Moreira,Marcela guerreira,mmoreira@gmail.com,APTO,INDEFERIDO,PARTIDO ISOLADO,45,PSDB,Partido Socialismo,Todos em nome da cidade,PSTU/PSB/PSL,UF,Maceio,1997-11-29T00:00:00Z,123456789,FEMININO,ENSINO MEDIO INCOMPLETO,CASADO,NEGRO,ARTISTA,SUPLENTE\n", string(b))
 }

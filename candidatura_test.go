65be140cf7b9fa3df117f7087ba5f4833aba01fd
abuarquemf <abuarquemf@gmail.com>
2020-06-03 16:27:01 -0300

    applied requested changes

--- candidatura_test.go
+++ candidatura_test.go
@@ -8,11 +8,6 @@ import (
 )
 
 func TestCandidatura(t *testing.T) {
-	is := is.New(t)
-	data, err := time.Parse("02-01-2006", "02-10-2016")
-	if err != nil {
-		t.Errorf("want error nil when creating a test date, got %q", err)
-	}
 	nascimento, err := time.Parse("02-01-2006", "29-11-1997")
 	if err != nil {
 		t.Errorf("want error nil when creating a test date for nascimento, got %q", err)
@@ -21,45 +16,46 @@ func TestCandidatura(t *testing.T) {
 		Legislatura:       2016,
 		Cargo:             ExecMunicipal,
 		UF:                "AL",
-		CPF:               "07496470430",
 		Turno:             1,
-		Descricacao:       "Eleições Municipais 2016",
-		Data:              data,
 		Municipio:         "Município",
 		NumeroUrna:        45555,
-		Nome:              "Aurélio Buarque de Miranda Filho",
 		NomeUrna:          "Lelinho do povo",
-		Email:             "abuarquemf@gmail.com",
-		Situacao:          "APTO",
-		SituacaoCandidato: "DEFERIDO",
+		Aptidao:           "APTO",
+		Situacao:          "DEFERIDO",
 		TipoAgremiacao:    "PARTIDO ISOLADO",
 		NumeroPartido:     45,
 		LegendaPartido:    "PSDB",
 		NomePartido:       "Partido Socilismo",
 		NomeColigacao:     "Todos pela cidade",
 		PartidosColigacao: "PSDB/PT/PSC",
-		UFOrigem:          "UF",
-		MunicipioOrigem:   "Maceio",
-		Nascimento:        nascimento,
-		TituloEleitoral:   "000000000",
-		Gênero:            "MASCULINO",
-		GrauInstrucao:     "ENSINO MEDIO COMPLETO",
-		EstadoCivil:       "SOLTEIRO",
-		Raca:              "PARDA",
-		OcupacaoCandidato: "ENGENHEIRO",
 		SituacaoFinal:     "ELEITO",
+		DeclarouBens:      true,
+		Candidato: Candidato{
+			Email:           "abuarquemf@gmail.com",
+			Nome:            "Aurélio Buarque de Miranda Filho",
+			CPF:             "07496470430",
+			UF:              "UF",
+			Municipio:       "Maceio",
+			Nascimento:      nascimento,
+			TituloEleitoral: "000000000",
+			Genero:          "MASCULINO",
+			GrauInstrucao:   "ENSINO MEDIO COMPLETO",
+			EstadoCivil:     "SOLTEIRO",
+			Raca:            "PARDA",
+			Ocupacao:        "ENGENHEIRO",
+		},
 	}
 	j, err := cand.ToJSON()
+	if err != nil {
+		t.Errorf("want error nil when passing struct candidato to JSON, got %q", err)
+	}
+	is := is.New(t)
 	is.NoErr(err)
-	is.Equal(`{"leg":2016,"cargo":"EM","uf":"AL","cpf":"07496470430","turno":1,"descricao":"Eleições Municipais 2016","data":"2016-10-02T00:00:00Z","municipio":"Município","numero_urna":45555,"nome":"Aurélio Buarque de Miranda Filho","nome_urna":"Lelinho do povo","email":"abuarquemf@gmail.com","situacao":"APTO","situacao_candidato":"DEFERIDO","tipo_agremiacao":"PARTIDO ISOLADO","numero_partio":45,"legenda_partido":"PSDB","nome_partido":"Partido Socilismo","nome_coligacao":"Todos pela cidade","partidos_coligacao":"PSDB/PT/PSC","uf_origem":"UF","municipio_origem":"Maceio","nascimento":"1997-11-29T00:00:00Z","titulo_eleitoral":"000000000","genero":"MASCULINO","grau_instrucao":"ENSINO MEDIO COMPLETO","estado_civil":"SOLTEIRO","raca":"PARDA","ocupacao":"ENGENHEIRO","situacao_final":"ELEITO"}`, string(j))
+	is.Equal(`{"leg":2016,"cargo":"EM","uf":"AL","turno":1,"municipio":"Município","numero_urna":45555,"nome_urna":"Lelinho do povo","aptidao":"APTO","situacao":"DEFERIDO","tipo_agremiacao":"PARTIDO ISOLADO","numero_partio":45,"legenda_partido":"PSDB","nome_partido":"Partido Socilismo","nome_coligacao":"Todos pela cidade","partidos_coligacao":"PSDB/PT/PSC","situacao_final":"ELEITO","declarou_bens":true,"uf_origem":"UF","municipio_origem":"Maceio","nascimento":"1997-11-29T00:00:00Z","titulo_eleitoral":"000000000","genero":"MASCULINO","grau_instrucao":"ENSINO MEDIO COMPLETO","estado_civil":"SOLTEIRO","raca":"PARDA","ocupacao":"ENGENHEIRO","cpf":"07496470430","nome":"Aurélio Buarque de Miranda Filho","email":"abuarquemf@gmail.com"}`, string(j))
 
 }
 
 func TestToCSV(t *testing.T) {
-	data, err := time.Parse("02-01-2006", "02-10-2016")
-	if err != nil {
-		t.Errorf("want error nil when creating a test date, got %q", err)
-	}
 	nascimento, err := time.Parse("02-01-2006", "29-11-1997")
 	if err != nil {
 		t.Errorf("want error nil when creating a test date for nascimento, got %q", err)
@@ -69,69 +65,74 @@ func TestToCSV(t *testing.T) {
 			Legislatura:       2016,
 			Cargo:             ExecMunicipal,
 			UF:                "AL",
-			CPF:               "07496470430",
 			Turno:             1,
-			Descricacao:       "Eleições Municipais 2016",
-			Data:              data,
 			Municipio:         "Município",
 			NumeroUrna:        45555,
-			Nome:              "Aurélio Buarque de Miranda Filho",
 			NomeUrna:          "Lelinho do povo",
-			Email:             "abuarquemf@gmail.com",
-			Situacao:          "APTO",
-			SituacaoCandidato: "DEFERIDO",
+			Aptidao:           "APTO",
+			Situacao:          "DEFERIDO",
 			TipoAgremiacao:    "PARTIDO ISOLADO",
 			NumeroPartido:     45,
 			LegendaPartido:    "PSDB",
-			NomePartido:       "Partido Socialismo",
+			NomePartido:       "Partido Socilismo",
 			NomeColigacao:     "Todos pela cidade",
 			PartidosColigacao: "PSDB/PT/PSC",
-			UFOrigem:          "UF",
-			MunicipioOrigem:   "Maceio",
-			Nascimento:        nascimento,
-			TituloEleitoral:   "000000000",
-			Gênero:            "MASCULINO",
-			GrauInstrucao:     "ENSINO MEDIO COMPLETO",
-			EstadoCivil:       "SOLTEIRO",
-			Raca:              "PARDA",
-			OcupacaoCandidato: "ENGENHEIRO",
 			SituacaoFinal:     "ELEITO",
+			DeclarouBens:      true,
+			Candidato: Candidato{
+				Email:           "abuarquemf@gmail.com",
+				Nome:            "Aurélio Buarque de Miranda Filho",
+				CPF:             "07496470430",
+				UF:              "UF",
+				Municipio:       "Maceio",
+				Nascimento:      nascimento,
+				TituloEleitoral: "000000000",
+				Genero:          "MASCULINO",
+				GrauInstrucao:   "ENSINO MEDIO COMPLETO",
+				EstadoCivil:     "SOLTEIRO",
+				Raca:            "PARDA",
+				Ocupacao:        "ENGENHEIRO",
+			},
 		},
 		{
 			Legislatura:       2016,
 			Cargo:             ExecMunicipal,
 			UF:                "AL",
-			CPF:               "00000000000",
 			Turno:             1,
-			Descricacao:       "Eleições Municipais 2016",
-			Data:              data,
 			Municipio:         "Município",
 			NumeroUrna:        45555,
-			Nome:              "Marcela Moreira",
-			NomeUrna:          "Marcela guerreira",
-			Email:             "mmoreira@gmail.com",
-			Situacao:          "APTO",
-			SituacaoCandidato: "INDEFERIDO",
+			NomeUrna:          "Lobao",
+			Aptidao:           "APTO",
+			Situacao:          "DEFERIDO",
 			TipoAgremiacao:    "PARTIDO ISOLADO",
 			NumeroPartido:     45,
-			LegendaPartido:    "PSDB",
-			NomePartido:       "Partido Socialismo",
-			NomeColigacao:     "Todos em nome da cidade",
-			PartidosColigacao: "PSTU/PSB/PSL",
-			UFOrigem:          "UF",
-			MunicipioOrigem:   "Maceio",
-			Nascimento:        nascimento,
-			TituloEleitoral:   "123456789",
-			Gênero:            "FEMININO",
-			GrauInstrucao:     "ENSINO MEDIO INCOMPLETO",
-			EstadoCivil:       "CASADO",
-			Raca:              "NEGRO",
-			OcupacaoCandidato: "ARTISTA",
-			SituacaoFinal:     "SUPLENTE",
+			LegendaPartido:    "PT",
+			NomePartido:       "Partido Trabalhista",
+			NomeColigacao:     "Cidade para frente",
+			PartidosColigacao: "PSDB/PT/PSC",
+			SituacaoFinal:     "ELEITO",
+			DeclarouBens:      false,
+			Candidato: Candidato{
+				Email:           "marcelo@gmail.com",
+				Nome:            "Anivaldo Silva Lobao",
+				CPF:             "11122233344456",
+				UF:              "UF",
+				Municipio:       "Maceio",
+				Nascimento:      nascimento,
+				TituloEleitoral: "123456789",
+				Genero:          "MASCULINO",
+				GrauInstrucao:   "ENSINO MEDIO INCOMPLETO",
+				EstadoCivil:     "CASADO",
+				Raca:            "NEGRO",
+				Ocupacao:        "ARTISTA",
+			},
 		},
 	}
-	b, _ := ToCSV(c)
+	b, err := ToCSV(c)
+	if err != nil {
+		t.Errorf("want error nil when passing struct candidato to CSV, got %q", err)
+	}
 	is := is.New(t)
 	is.NoErr(err)
-	is.Equal("leg,cargo,uf,cpf,turno,desricao,data,municipio,numero_urna,nome,nome_urna,email,situacao,situacao_candidato,tipo_agremiacao,numero_partido,legenda_partido,nome_partido,nome_coligacao,partidos_coligacao,uf_origem,municipio_origem,nascimento,titulo_eleitoral,genero,grau_instrucao,estado_civil,raca,ocupacao,situacao_final\n2016,EM,AL,07496470430,1,Eleições Municipais 2016,2016-10-02T00:00:00Z,Município,45555,Aurélio Buarque de Miranda Filho,Lelinho do povo,abuarquemf@gmail.com,APTO,DEFERIDO,PARTIDO ISOLADO,45,PSDB,Partido Socialismo,Todos pela cidade,PSDB/PT/PSC,UF,Maceio,1997-11-29T00:00:00Z,000000000,MASCULINO,ENSINO MEDIO COMPLETO,SOLTEIRO,PARDA,ENGENHEIRO,ELEITO\n2016,EM,AL,00000000000,1,Eleições Municipais 2016,2016-10-02T00:00:00Z,Município,45555,Marcela Moreira,Marcela guerreira,mmoreira@gmail.com,APTO,INDEFERIDO,PARTIDO ISOLADO,45,PSDB,Partido Socialismo,Todos em nome da cidade,PSTU/PSB/PSL,UF,Maceio,1997-11-29T00:00:00Z,123456789,FEMININO,ENSINO MEDIO INCOMPLETO,CASADO,NEGRO,ARTISTA,SUPLENTE\n", string(b))
+	is.Equal("leg,cargo,uf,turno,municipio,numero_urna,nome_urna,aptidao,situacao,tipo_agremiacao,numero_partido,legenda_partido,nome_partido,nome_coligacao,partidos_coligacao,situacao_final,declarou_bens,uf_origem,municipio_origem,nascimento,titulo_eleitoral,genero,grau_instrucao,estado_civil,raca,ocupacao,cpf,nome,email\n2016,EM,AL,1,Município,45555,Lelinho do povo,APTO,DEFERIDO,PARTIDO ISOLADO,45,PSDB,Partido Socilismo,Todos pela cidade,PSDB/PT/PSC,ELEITO,true,UF,Maceio,1997-11-29T00:00:00Z,000000000,MASCULINO,ENSINO MEDIO COMPLETO,SOLTEIRO,PARDA,ENGENHEIRO,07496470430,Aurélio Buarque de Miranda Filho,abuarquemf@gmail.com\n2016,EM,AL,1,Município,45555,Lobao,APTO,DEFERIDO,PARTIDO ISOLADO,45,PT,Partido Trabalhista,Cidade para frente,PSDB/PT/PSC,ELEITO,false,UF,Maceio,1997-11-29T00:00:00Z,123456789,MASCULINO,ENSINO MEDIO INCOMPLETO,CASADO,NEGRO,ARTISTA,11122233344456,Anivaldo Silva Lobao,marcelo@gmail.com\n", string(b))
 }

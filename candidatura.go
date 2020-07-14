038204ac434dca10be3b053e26e1b2d92b7ee83e
abuarquemf <abuarquemf@gmail.com>
2020-07-14 19:39:07 -0300

    added sequencial candidate to struct

--- candidatura.go
+++ candidatura.go
@@ -34,23 +34,24 @@ type Candidato struct {
 
 // Candidatura representa uma
 type Candidatura struct {
-	Legislatura           int    `json:"leg" csv:"leg"`                               // Ano eleitoral em que a candidatura foi homologada.
-	Cargo                 Cargo  `json:"cargo" csv:"cargo"`                           // Cargo sendo pleiteado pela candidatura.
-	UF                    string `json:"uf" csv:"uf"`                                 // Identificador (2 caracteres) de unidade federativa onde ocorreu a candidatura.
-	Municipio             string `json:"municipio" csv:"municipio"`                   // Município que ocorreu a eleição.
-	NumeroUrna            int    `json:"numero_urna" csv:"numero_urna"`               // Número do candidato na urna.
-	NomeUrna              string `json:"nome_urna" csv:"nome_urna"`                   // Nome do candidato na urna.
-	Aptidao               string `json:"aptidao" csv:"aptidao"`                       // Aptidao da candidatura (podendo ser APTO ou INAPTO).
-	Deferimento           string `json:"deferimento" csv:"deferimento"`               // Situação do candidato (pondendo ser DEFERIDO ou INDEFERIDO).
-	TipoAgremiacao        string `json:"tipo_agremiacao" csv:"tipo_agremiacao"`       // Indica o tipo de agremiação do candidato (podendo ser PARTIDO ISOLADO ou AGREMIAÇÃO).
-	NumeroPartido         int    `json:"numero_partio" csv:"numero_partido"`          // Número do partido do candidato.
-	LegendaPartido        string `json:"legenda_partido" csv:"legenda_partido"`       // Legenda do partido do candidato.
-	NomePartido           string `json:"nome_partido" csv:"nome_partido"`             // Nome do partido do candidato.
-	NomeColigacao         string `json:"nome_coligacao" csv:"nome_coligacao"`         // Nome da coligação a qual o candidato pertence.
-	PartidosColigacao     string `json:"partidos_coligacao" csv:"partidos_coligacao"` // Partidos pertencentes à coligação do candidato.
-	DeclarouBens          bool   `json:"declarou_bens" csv:"declarou_bens"`           // Flag que informa se o candidato declarou seus bens na eleição.s
-	SituacaoPrimeiroTurno string `json:"situacao_1turno" csv:"situacao_1turno"`       // Campo que informa como o candidato terminou o primeiro turno da eleição (por exemplo como ELEITO, NÃO ELEITO, ELEITO POR MÉDIA) ou se foi para o segundo turno (ficando com situação SEGUNDO TURNO).
-	SituacaoSegundoTurno  string `json:"situacao_2turno" csv:"situacao_2turno"`       // Campo que informa como o candidato terminou o segundo turno da eleição, nesse caso só pondendo ficar como ELEITO ou NÃO ELEITO.
+	Legislatura           int    `json:"leg" csv:"leg"`                                   // Ano eleitoral em que a candidatura foi homologada.
+	Cargo                 Cargo  `json:"cargo" csv:"cargo"`                               // Cargo sendo pleiteado pela candidatura.
+	UF                    string `json:"uf" csv:"uf"`                                     // Identificador (2 caracteres) de unidade federativa onde ocorreu a candidatura.
+	Municipio             string `json:"municipio" csv:"municipio"`                       // Município que ocorreu a eleição.
+	NumeroUrna            int    `json:"numero_urna" csv:"numero_urna"`                   // Número do candidato na urna.
+	NomeUrna              string `json:"nome_urna" csv:"nome_urna"`                       // Nome do candidato na urna.
+	Aptidao               string `json:"aptidao" csv:"aptidao"`                           // Aptidao da candidatura (podendo ser APTO ou INAPTO).
+	Deferimento           string `json:"deferimento" csv:"deferimento"`                   // Situação do candidato (pondendo ser DEFERIDO ou INDEFERIDO).
+	TipoAgremiacao        string `json:"tipo_agremiacao" csv:"tipo_agremiacao"`           // Indica o tipo de agremiação do candidato (podendo ser PARTIDO ISOLADO ou AGREMIAÇÃO).
+	NumeroPartido         int    `json:"numero_partio" csv:"numero_partido"`              // Número do partido do candidato.
+	LegendaPartido        string `json:"legenda_partido" csv:"legenda_partido"`           // Legenda do partido do candidato.
+	NomePartido           string `json:"nome_partido" csv:"nome_partido"`                 // Nome do partido do candidato.
+	NomeColigacao         string `json:"nome_coligacao" csv:"nome_coligacao"`             // Nome da coligação a qual o candidato pertence.
+	PartidosColigacao     string `json:"partidos_coligacao" csv:"partidos_coligacao"`     // Partidos pertencentes à coligação do candidato.
+	DeclarouBens          bool   `json:"declarou_bens" csv:"declarou_bens"`               // Flag que informa se o candidato declarou seus bens na eleição.s
+	SituacaoPrimeiroTurno string `json:"situacao_1turno" csv:"situacao_1turno"`           // Campo que informa como o candidato terminou o primeiro turno da eleição (por exemplo como ELEITO, NÃO ELEITO, ELEITO POR MÉDIA) ou se foi para o segundo turno (ficando com situação SEGUNDO TURNO).
+	SituacaoSegundoTurno  string `json:"situacao_2turno" csv:"situacao_2turno"`           // Campo que informa como o candidato terminou o segundo turno da eleição, nesse caso só pondendo ficar como ELEITO ou NÃO ELEITO.
+	SequencialCandidato   string `json:"sequencial_candidato" csv:"sequencial_candidato"` // ID sequencial do candidato no sistema do TSE
 	Candidato             `json:"candidato"`
 }
 
--- candidatura_test.go
+++ candidatura_test.go
@@ -30,6 +30,7 @@ func TestCandidatura(t *testing.T) {
 		SituacaoSegundoTurno:  "ELEITO",
 		SituacaoPrimeiroTurno: "SEGUNDO TURNO",
 		DeclarouBens:          true,
+		SequencialCandidato:   "505",
 		Candidato: Candidato{
 			Email:           "abuarquemf@gmail.com",
 			Nome:            "Aurélio Buarque de Miranda Filho",
@@ -51,7 +52,7 @@ func TestCandidatura(t *testing.T) {
 	}
 	is := is.New(t)
 	is.NoErr(err)
-	is.Equal(`{"leg":2016,"cargo":"EM","uf":"AL","municipio":"Município","numero_urna":45555,"nome_urna":"Lelinho do povo","aptidao":"APTO","deferimento":"DEFERIDO","tipo_agremiacao":"PARTIDO ISOLADO","numero_partio":45,"legenda_partido":"PSDB","nome_partido":"Partido Socilismo","nome_coligacao":"Todos pela cidade","partidos_coligacao":"PSDB/PT/PSC","declarou_bens":true,"situacao_1turno":"SEGUNDO TURNO","situacao_2turno":"ELEITO","candidato":{"uf_origem":"UF","municipio_origem":"Maceio","nascimento":"1997-11-29T00:00:00Z","titulo_eleitoral":"000000000","genero":"MASCULINO","grau_instrucao":"ENSINO MEDIO COMPLETO","estado_civil":"SOLTEIRO","raca":"PARDA","ocupacao":"ENGENHEIRO","cpf":"07496470430","nome":"Aurélio Buarque de Miranda Filho","email":"abuarquemf@gmail.com"}}`, string(j))
+	is.Equal(`{"leg":2016,"cargo":"EM","uf":"AL","municipio":"Município","numero_urna":45555,"nome_urna":"Lelinho do povo","aptidao":"APTO","deferimento":"DEFERIDO","tipo_agremiacao":"PARTIDO ISOLADO","numero_partio":45,"legenda_partido":"PSDB","nome_partido":"Partido Socilismo","nome_coligacao":"Todos pela cidade","partidos_coligacao":"PSDB/PT/PSC","declarou_bens":true,"situacao_1turno":"SEGUNDO TURNO","situacao_2turno":"ELEITO","sequencial_candidato":"505","candidato":{"uf_origem":"UF","municipio_origem":"Maceio","nascimento":"1997-11-29T00:00:00Z","titulo_eleitoral":"000000000","genero":"MASCULINO","grau_instrucao":"ENSINO MEDIO COMPLETO","estado_civil":"SOLTEIRO","raca":"PARDA","ocupacao":"ENGENHEIRO","cpf":"07496470430","nome":"Aurélio Buarque de Miranda Filho","email":"abuarquemf@gmail.com"}}`, string(j))
 }
 
 func TestToCSV(t *testing.T) {
@@ -78,6 +79,7 @@ func TestToCSV(t *testing.T) {
 			DeclarouBens:          true,
 			SituacaoSegundoTurno:  "ELEITO",
 			SituacaoPrimeiroTurno: "SEGUNDO TURNO",
+			SequencialCandidato:   "505",
 			Candidato: Candidato{
 				Email:           "abuarquemf@gmail.com",
 				Nome:            "Aurélio Buarque de Miranda Filho",
@@ -111,6 +113,7 @@ func TestToCSV(t *testing.T) {
 			DeclarouBens:          true,
 			SituacaoSegundoTurno:  "",
 			SituacaoPrimeiroTurno: "ELEITO",
+			SequencialCandidato:   "505",
 			Candidato: Candidato{
 				Email:           "abuarquemf@gmail.com",
 				Nome:            "Aurélio Buarque de Miranda Filho",
@@ -133,5 +136,5 @@ func TestToCSV(t *testing.T) {
 	}
 	is := is.New(t)
 	is.NoErr(err)
-	is.Equal("leg,cargo,uf,municipio,numero_urna,nome_urna,aptidao,deferimento,tipo_agremiacao,numero_partido,legenda_partido,nome_partido,nome_coligacao,partidos_coligacao,declarou_bens,situacao_1turno,situacao_2turno,uf_origem,municipio_origem,nascimento,titulo_eleitoral,genero,grau_instrucao,estado_civil,raca,ocupacao,cpf,nome,email\n2016,EM,AL,Município,45555,Lelinho do povo,APTO,DEFERIDO,PARTIDO ISOLADO,45,PSDB,Partido Socilismo,Todos pela cidade,PSDB/PT/PSC,true,SEGUNDO TURNO,ELEITO,UF,Maceio,1997-11-29T00:00:00Z,000000000,MASCULINO,ENSINO MEDIO COMPLETO,SOLTEIRO,PARDA,ENGENHEIRO,07496470430,Aurélio Buarque de Miranda Filho,abuarquemf@gmail.com\n2016,EM,AL,Município,45555,Lelinho do povo,APTO,DEFERIDO,PARTIDO ISOLADO,45,PSDB,Partido Socilismo,Todos pela cidade,PSDB/PT/PSC,true,ELEITO,,UF,Maceio,1997-11-29T00:00:00Z,000000000,MASCULINO,ENSINO MEDIO COMPLETO,SOLTEIRO,PARDA,ENGENHEIRO,07496470430,Aurélio Buarque de Miranda Filho,abuarquemf@gmail.com\n", string(b))
+	is.Equal("leg,cargo,uf,municipio,numero_urna,nome_urna,aptidao,deferimento,tipo_agremiacao,numero_partido,legenda_partido,nome_partido,nome_coligacao,partidos_coligacao,declarou_bens,situacao_1turno,situacao_2turno,sequencial_candidato,uf_origem,municipio_origem,nascimento,titulo_eleitoral,genero,grau_instrucao,estado_civil,raca,ocupacao,cpf,nome,email\n2016,EM,AL,Município,45555,Lelinho do povo,APTO,DEFERIDO,PARTIDO ISOLADO,45,PSDB,Partido Socilismo,Todos pela cidade,PSDB/PT/PSC,true,SEGUNDO TURNO,ELEITO,505,UF,Maceio,1997-11-29T00:00:00Z,000000000,MASCULINO,ENSINO MEDIO COMPLETO,SOLTEIRO,PARDA,ENGENHEIRO,07496470430,Aurélio Buarque de Miranda Filho,abuarquemf@gmail.com\n2016,EM,AL,Município,45555,Lelinho do povo,APTO,DEFERIDO,PARTIDO ISOLADO,45,PSDB,Partido Socilismo,Todos pela cidade,PSDB/PT/PSC,true,ELEITO,,505,UF,Maceio,1997-11-29T00:00:00Z,000000000,MASCULINO,ENSINO MEDIO COMPLETO,SOLTEIRO,PARDA,ENGENHEIRO,07496470430,Aurélio Buarque de Miranda Filho,abuarquemf@gmail.com\n", string(b))
 }

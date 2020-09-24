a43297a7bc780dd717e7a2de4516f88f36163d32
abuarquemf <abuarquemf@gmail.com>
2020-09-24 08:50:07 -0300

    feat: added bson annoations

--- candidature_for_db.go
+++ candidature_for_db.go
@@ -24,34 +24,34 @@ type VotingCity struct {
 // CandidateForDB é uma struct que contem alguns dos atributos da struct Candidatura.
 // Essa struct é usada somente para persistência em banco e para atender requisitos da UI do site.
 type CandidateForDB struct {
-	SequencialCandidate string   `datastore:"sequencial_candidate,omitempty"` // ID sequencial do candidato no sistema do TSE.
-	Description         string   `datastore:"description,omitempty"`          // Description do candidato.
-	Biography           string   `datastore:"biography,omitempty"`            // Biography do candidato.
-	PhotoURL            string   `datastore:"photo_url,omitempty"`            // URL da foto do candidato.
-	LegalCode           string   `datastore:"legal_code,omitempty"`           // CPF do candidato.
-	Party               string   `datastore:"party,omitempty"`                // Partido do candidato.
-	Name                string   `datastore:"name,omitempty"`                 // Nome natural de pessoa física do candidato.
-	BallotName          string   `datastore:"ballot_name,omitempty"`          // Nome do candidato na urna.
-	BallotNumber        int      `datastore:"ballot_number,omitempty"`        // Número do candidato na urna.
-	Email               string   `datastore:"email,omitempty"`                // Email do candidato.
-	Role                string   `datastore:"role,omitempty"`                 // Cargo do candidato (como vereador ou prefeito).
-	State               string   `datastore:"state,omitempty"`                // Estado da eleição.
-	City                string   `datastore:"city,omitempty"`                 // Cidade da eleição.
-	Year                int      `datastore:"year,omitempty"`                 // Ano da eleição.
-	Tags                []string `datastore:"tags,omitempty"`                 // Tags do candidato.
-	Gender              string   `datastore:"gender,omitempty"`               // Gênero do candidato.
-	Transparence        float64  `datastore:"transparence,omitempty"`         // Porcentagem de transparência do candidato.
-	Contact             *Contact `datastore:"contact,omitempty"`              // Dados de contato do candidato.
+	SequencialCandidate string   `datastore:"sequencial_candidate,omitempty" bson:"sequencial_candidate,omitempty"` // ID sequencial do candidato no sistema do TSE.
+	Description         string   `datastore:"description,omitempty" bson:"description,omitempty"`                   // Description do candidato.
+	Biography           string   `datastore:"biography,omitempty" bson:"biography,omitempty"`                       // Biography do candidato.
+	PhotoURL            string   `datastore:"photo_url,omitempty" bson:"photo_url,omitempty"`                       // URL da foto do candidato.
+	LegalCode           string   `datastore:"legal_code,omitempty" bson:"legal_code,omitempty"`                     // CPF do candidato.
+	Party               string   `datastore:"party,omitempty" bson:"party,omitempty"`                               // Partido do candidato.
+	Name                string   `datastore:"name,omitempty" bson:"name,omitempty"`                                 // Nome natural de pessoa física do candidato.
+	BallotName          string   `datastore:"ballot_name,omitempty" bson:"ballot_name,omitempty"`                   // Nome do candidato na urna.
+	BallotNumber        int      `datastore:"ballot_number,omitempty" bson:"ballot_number,omitempty"`               // Número do candidato na urna.
+	Email               string   `datastore:"email,omitempty" bson:"email,omitempty"`                               // Email do candidato.
+	Role                string   `datastore:"role,omitempty" bson:"role,omitempty"`                                 // Cargo do candidato (como vereador ou prefeito).
+	State               string   `datastore:"state,omitempty" bson:"state,omitempty"`                               // Estado da eleição.
+	City                string   `datastore:"city,omitempty" bson:"city,omitempty"`                                 // Cidade da eleição.
+	Year                int      `datastore:"year,omitempty" bson:"year,omitempty"`                                 // Ano da eleição.
+	Tags                []string `datastore:"tags,omitempty" bson:"tags,omitempty"`                                 // Tags do candidato.
+	Gender              string   `datastore:"gender,omitempty" bson:"gender,omitempty"`                             // Gênero do candidato.
+	Transparence        float64  `datastore:"transparence,omitempty" bson:"transparence,omitempty"`                 // Porcentagem de transparência do candidato.
+	Contact             *Contact `datastore:"contact,omitempty" bson:"contact,omitempty"`                           // Dados de contato do candidato.
 }
 
 // Contact é um struct para armazenar os dados de contato do candidato.
 type Contact struct {
-	IconURL string `datastore:"icon_url,omitempty"` // Ícone do contato, podendo ser o link para a logo do Instagram, ou Facebook...
-	Link    string `datastore:"link,omitempty"`     // Link de contato, podendo ser o link para o Instagram, um site, ou o telefone do candidato.
+	IconURL string `datastore:"icon_url,omitempty" bson:"icon_url,omitempty"` // Ícone do contato, podendo ser o link para a logo do Instagram, ou Facebook...
+	Link    string `datastore:"link,omitempty" bson:"link,omitempty"`         // Link de contato, podendo ser o link para o Instagram, um site, ou o telefone do candidato.
 }
 
 // Location é uma struct que contem um estado que está ocorrendo a eleição e suas cidades.
 type Location struct {
-	State  string   `datastore:"state"`  // Estado que está ocorrendo uma eleição.
-	Cities []string `datastore:"cities"` // Cidades do estado onde está ocorrendo uma eleição.
+	State  string   `datastore:"state" bson:"link,omitempty"` // Estado que está ocorrendo uma eleição.
+	Cities []string `datastore:"cities" bson:"cities"`        // Cidades do estado onde está ocorrendo uma eleição.
 }
